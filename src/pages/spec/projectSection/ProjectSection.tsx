eaa2a15accd007dcbed9740bf6874e8911ee633e
이서준 <104981505+xeoxxn@users.noreply.github.com>
2026-02-13 18:40:39 +0900

    프로젝트 섹션 컴포넌트 분리

--- src/pages/spec/projectSection/ProjectSection.tsx
+++ src/pages/spec/projectSection/ProjectSection.tsx
@@ -1,72 +1,29 @@
-export default function ProjectSection() {
+import ProjectCard from './ProjectCard.tsx';
+
+interface Project {
+  title: string;
+  description: string;
+  period: string;
+  role: string;
+  achievements: string;
+}
+
+interface ProjectSectionProps {
+  projects: Project[];
+}
+export default function ProjectSection({ projects }: ProjectSectionProps) {
   return (
     <div className="inline-flex w-full flex-col items-start justify-start">
-      <div className="flex w-full flex-col items-start justify-center gap-2.5 border-b border-neutral-400/40 px-12 py-5">
-        <div className="inline-flex items-start justify-start gap-10">
-          <div className="relative h-40 w-48 rounded-[10px] bg-gray-100"></div>
-          <div className="inline-flex flex-col items-start justify-center gap-3 self-stretch">
-            <div className="justify-start font-['Pretendard_Variable'] text-lg leading-7 font-semibold text-black">
-              프로젝트 이름
-            </div>
-            <div className="flex flex-col items-start justify-start gap-2">
-              <div className="justify-start font-['Pretendard_Variable'] text-sm leading-5 font-medium text-black">
-                친구들과 즐기는 자유로운 개인 도네이션 서비스 <br />
-                원하는 선물을 직접 요구하고, 후원을 통해 선물을 줄 수 있는 앱
-              </div>
-              <div className="justify-start text-center font-['Pretendard_Variable'] text-sm leading-5 font-medium text-black">
-                역할 : 프론트엔드
-              </div>
-              <div className="justify-start font-['Pretendard_Variable'] text-sm leading-5 font-medium text-black">
-                2026.01.21 - 2026.05.20
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex w-full flex-col items-start justify-center gap-2.5 border-b border-neutral-400/40 px-12 py-5">
-        <div className="inline-flex items-start justify-start gap-10">
-          <div className="relative h-40 w-48 rounded-[10px] bg-gray-100"></div>
-          <div className="inline-flex flex-col items-start justify-center gap-3 self-stretch">
-            <div className="justify-start font-['Pretendard_Variable'] text-lg leading-7 font-semibold text-black">
-              프로젝트 이름
-            </div>
-            <div className="flex flex-col items-start justify-start gap-2">
-              <div className="justify-start font-['Pretendard_Variable'] text-sm leading-5 font-medium text-black">
-                친구들과 즐기는 자유로운 개인 도네이션 서비스 <br />
-                원하는 선물을 직접 요구하고, 후원을 통해 선물을 줄 수 있는 앱
-              </div>
-              <div className="justify-start text-center font-['Pretendard_Variable'] text-sm leading-5 font-medium text-black">
-                역할 : 프론트엔드
-              </div>
-              <div className="justify-start font-['Pretendard_Variable'] text-sm leading-5 font-medium text-black">
-                2026.01.21 - 2026.05.20
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex w-full flex-col items-start justify-center gap-2.5 border-b border-neutral-400/40 px-12 py-5">
-        <div className="inline-flex items-start justify-start gap-10">
-          <div className="relative h-40 w-48 rounded-[10px] bg-gray-100"></div>
-          <div className="inline-flex flex-col items-start justify-center gap-3 self-stretch">
-            <div className="justify-start font-['Pretendard_Variable'] text-lg leading-7 font-semibold text-black">
-              프로젝트 이름
-            </div>
-            <div className="flex flex-col items-start justify-start gap-2">
-              <div className="justify-start font-['Pretendard_Variable'] text-sm leading-5 font-medium text-black">
-                친구들과 즐기는 자유로운 개인 도네이션 서비스 <br />
-                원하는 선물을 직접 요구하고, 후원을 통해 선물을 줄 수 있는 앱
-              </div>
-              <div className="justify-start text-center font-['Pretendard_Variable'] text-sm leading-5 font-medium text-black">
-                역할 : 프론트엔드
-              </div>
-              <div className="justify-start font-['Pretendard_Variable'] text-sm leading-5 font-medium text-black">
-                2026.01.21 - 2026.05.20
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {projects.map((project, index) => (
+        <ProjectCard
+          key={index}
+          title={project.title}
+          description={project.description}
+          period={project.period}
+          role={project.role}
+          achievements={project.achievements}
+        />
+      ))}
     </div>
   );
 }
